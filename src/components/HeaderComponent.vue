c8320c7491e00cb519c2b425692d326bef9c4979
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-14 12:17:50 +0200

    add-img-add-svg

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header__component">
+    <div class="btn">
+      <button>order online</button>
+    </div>
+    <div class="main_nav">
+      <ul>
+        <li>
+          <a href="#">home</a>
+        </li>
+        <li>
+          <a href="#">pages</a>
+        </li>
+        <li>
+          <a href="#">menù</a>
+        </li>
+        <li>
+          <a href="#"><img src="../assets/img/h5-logo-divided-header.png" alt=""></a>
+        </li>
+        <li>
+          <a href="#">event</a>
+        </li>
+        <li>
+          <a href="#">blog</a>
+        </li>
+        <li>
+          <a href="#">landing</a>
+        </li>   
+      </ul>
+    </div>
+    <div class="search_nav">
+      <ul>
+        <li>
+
+        </li>
+        <li>
+          
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
