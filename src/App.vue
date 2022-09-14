ba08204a22077757098b54eb606c403b68c706c7
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-14 15:57:46 +0200

    init-footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <HeaderComponent/>
+    <FooterComponent/>
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 
 
 export default {
   name: 'App',
   components: {
-    HeaderComponent
-}
+    HeaderComponent,
+    FooterComponent
+  }
 }
 </script>
 
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="main__footer flex">
+    <div class="footer__link flex">
+      <ul class="our__restaurant">
+        <h3>find our restaurants</h3>
+        <li>
+          <p class="address">1614 E. Bell rd#104</p>
+          <p class="city">Salerno, AZ85022</p>
+          <p class="number">(602)867-1010</p>
+        </li>
+      </ul>
+      <ul class="working__hours">
+        <h3>working hours</h3>
+        <li>
+          <p class="day">monday</p>
+          <p class="hours">Kitchen Closed</p>
+        </li>
+      </ul>
+      <p>the don peppe crew first and foremost values an authentic, well baked pizza</p>
+    </div>
+    <div class="img__footer">
+     
+    </div>
+  
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/footer';
+
+</style>
